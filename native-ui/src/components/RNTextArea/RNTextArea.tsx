e2aba9744cf30c5968955a02a51b6da1479d0a4e
Léo Fernandes <leandroleobh@gmail.com>
2024-01-09 14:12:36 -0300

    libraries updated

--- native-ui/src/components/RNTextArea/RNTextArea.tsx
+++ native-ui/src/components/RNTextArea/RNTextArea.tsx
@@ -1,12 +1,14 @@
 import { TextInput } from 'react-native'
 import { CustomTextAreaProps } from '../../styles/Types/ui-components.types'
 import { makeBaseTextAreaStyle } from '../../styles/styles.factory'
+import { forwardRef } from 'react'
 
-const RNTextArea = (props: CustomTextAreaProps) => {
+const RNTextArea = (props: CustomTextAreaProps, ref: any) => {
   const baseStyle = props.style ?? makeBaseTextAreaStyle(props)
 
   return (
     <TextInput
+      ref={ref || undefined}
       style={baseStyle}
       {...props}
       multiline
@@ -15,4 +17,4 @@ const RNTextArea = (props: CustomTextAreaProps) => {
   )
 }
 
-export default RNTextArea
+export default forwardRef<TextInput, CustomTextAreaProps>(RNTextArea)
